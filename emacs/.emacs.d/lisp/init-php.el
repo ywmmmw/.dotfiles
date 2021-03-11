60f9201899764288eac9c6c4bd5926f38248d4df
ywmmmw <ywmmmw@icloud.com>
2021-03-11 09:56:22 +0800

    update

--- emacs/.emacs.d/lisp/init-php.el
+++ emacs/.emacs.d/lisp/init-php.el
@@ -1,9 +1,15 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
-(use-package php-mode :ensure t)
+;(use-package php-mode :ensure t)
 
-(use-package web-mode :ensure t)
+(use-package web-mode
+  :ensure t
+  :mode (
+	 ".php$"
+	 ".html?$"
+	 )
+  )
 
 (provide 'init-php)
 
